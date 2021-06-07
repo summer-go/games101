9a022e993109290f72d474dcb15c18105412b2ac
sumsmile <xiatian20010@126.com>
2021-06-07 22:27:28 +0800

    Add files via upload

---
+++ Assignment3/Code/build/CMakeFiles/Rasterizer.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/baidu/Documents/study/games101/Assignment3/Code/Texture.cpp" "/Users/baidu/Documents/study/games101/Assignment3/Code/build/CMakeFiles/Rasterizer.dir/Texture.cpp.o"
+  "/Users/baidu/Documents/study/games101/Assignment3/Code/Triangle.cpp" "/Users/baidu/Documents/study/games101/Assignment3/Code/build/CMakeFiles/Rasterizer.dir/Triangle.cpp.o"
+  "/Users/baidu/Documents/study/games101/Assignment3/Code/main.cpp" "/Users/baidu/Documents/study/games101/Assignment3/Code/build/CMakeFiles/Rasterizer.dir/main.cpp.o"
+  "/Users/baidu/Documents/study/games101/Assignment3/Code/rasterizer.cpp" "/Users/baidu/Documents/study/games101/Assignment3/Code/build/CMakeFiles/Rasterizer.dir/rasterizer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar/eigen/3.3.9/include"
+  "/usr/local/include"
+  ".././include"
+  "/usr/local/Cellar/opencv/4.5.2/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
